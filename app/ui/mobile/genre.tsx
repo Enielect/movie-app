f8e245986f02b2c44b2e419304b041f2a62b6e5f
eniola <eniolafaithful57@gmail.com>
2024-05-05 13:44:18 +0100

    improved accessibility

--- app/ui/mobile/genre.tsx
+++ app/ui/mobile/genre.tsx
@@ -2,8 +2,8 @@ import { ReactElement } from "react";
 
 export default function Genre({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-yellow-300 py-3 px-4 rounded-full w-fit  text-black">
+    <button className="bg-yellow-300 py-3 px-4 rounded-full w-fit  text-black">
       {children}
-    </div>
+    </button>
   );
 }
