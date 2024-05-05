53785d53378683b92556648ed25fdcfe40c6f793
eniola <eniolafaithful57@gmail.com>
2024-05-05 12:44:05 +0100

    fixed rating and name of movie in a card

--- app/ui/movie-card.tsx
+++ app/ui/movie-card.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+const movie = {
+  rating: 9,
+  name: "Fighter",
+};
 export default function MovieCard() {
   return (
     <div>
@@ -7,9 +11,19 @@ export default function MovieCard() {
         style={{ backgroundImage: "url('movie.jpeg')" }}
         className="bg-image bg-cover bg-top relative rounded-lg w-full h-[400px] "
       >
-        <div className="px-[10px] flex w-full justify-between absolute bottom-0">
-          <span>movie_name</span>
-          <span>rating</span>
+        <div className="px-[10px] flex w-full justify-between pb-[10px] absolute bottom-0">
+          <span className="font-bold text-lg tracking-wide">{movie.name}</span>
+          <span
+            className={`${
+              movie.rating <= 5
+                ? "text-red-600"
+                : movie.rating > 5 && movie.rating < 8
+                ? "text-blue-600"
+                : "text-green-500"
+            } bg-black px-4 font-bold text-xl py-2 rounded-full`}
+          >
+            {movie.rating}
+          </span>
         </div>
       </div>
     </div>
