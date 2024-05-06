14909e6d3ad6c47178099cc27be1261b799cbaef
eniola <eniolafaithful57@gmail.com>
2024-05-06 14:47:30 +0100

    implemented the funtionality of opening and closing the sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import GenreList from "@/app/ui/mobile/genre-list";
 import SearchMobile from "@/app/ui/mobile/search";
 import MovieCard from "@/app/ui/movie-card";
 import Button from "@/app/ui/Navigate_button";
+import SideMenu from "@/app/ui/mobile/side-menu";
+import { useState } from "react";
 
 export default function Page() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="bg-white">
+      {/* add the side menu */}
+      {isModalOpen && <SideMenu />}
       <SearchMobile />
       <GenreList />
       <div className="mx-[15px] space-y-[10px]">
@@ -13,16 +20,28 @@ export default function Page() {
         {Array.from({ length: 20 }, (_, i) => (
           <MovieCard key={i} />
         ))}
-        <Button className=" left-[15px] bg-blue-600">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            style={{ fill: "rgba(0, 0, 0, 1);transform: ;msFilter:;" }}
-          >
-            <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
-          </svg>
+        <Button className=" left-[15px] bg-blue-600 z-20" onClick={() => setIsModalOpen(c => !c)}>
+          {!isModalOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: "rgba(0, 0, 0, 1);transform: ;msFilter:;" }}
+            >
+              <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: "rgba(0, 0, 0, 1);transform: ;msFilter:;" }}
+            >
+              <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+            </svg>
+          )}
         </Button>
         <Button className="right-[15px] bg-red-300">
           <svg
