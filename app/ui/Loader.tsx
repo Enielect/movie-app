173160c9563dddf9acab106458dd86b4db550657
eniola <eniolafaithful57@gmail.com>
2024-05-10 13:28:12 +0100

    created a loader using syled componentes

--- app/ui/Loader.tsx
+++ app/ui/Loader.tsx
@@ -2,7 +2,7 @@ import styles from "./spinner.module.css";
 
 export default function Loader() {
   return (
-    <div>
+    <div className="grid place-items-center">
       <div className={styles.spinner}></div>
     </div>
   );
