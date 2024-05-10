e9b81fc1691c916003f11630b80b88ee84e9bc4c
eniola <eniolafaithful57@gmail.com>
2024-05-10 13:27:49 +0100

    fixed my context api

--- app/contexts/GenreProvider.tsx
+++ app/contexts/GenreProvider.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import { createContext, useContext, useState, ReactNode } from "react";
-
-interface GenreContextType {
-  genreId: number;
-  setGenreId: React.Dispatch<React.SetStateAction<number>>;
-}
+import { createContext, useState } from "react";
 
 export const Context = createContext<GenreContextType | null>(null);
 
@@ -15,14 +10,10 @@ export default function GenreProvider({
   children: React.ReactNode;
 }) {
   const [genreId, setGenreId] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
   return (
-    <Context.Provider value={{ genreId, setGenreId }}>
+    <Context.Provider value={{ genreId, setGenreId, loading, setLoading }}>
       {children}
     </Context.Provider>
   );
 }
-
-export const useGenre = () => {
-  const context = useContext(Context);
-  return context;
-};
