59904bcf1e1e8022bcf0c2e5be4d28505f888512
eniola <eniolafaithful57@gmail.com>
2024-05-13 10:46:19 +0100

    update the trending page to fetch trending movies

--- app/trending/page.tsx
+++ app/trending/page.tsx
@@ -1,10 +1,22 @@
 import { GoBack } from "@/app/ui/back";
+import { fetchPopularMovies } from "@/tmdb";
+import MovieCard from "../ui/movie-card";
 
-export default function Page() {
+export default async function Page() {
+  //fetch trending movies (popular movies)
+  const movies = await fetchPopularMovies();
+
   return (
     <div>
       <GoBack />
-      This is the trending page
+      {movies.map((movie: Movies) => (
+        <MovieCard
+          key={movie.id}
+          src={movie.poster_path}
+          rating={movie.vote_average}
+          title={movie.title}
+        />
+      ))}
     </div>
   );
 }
