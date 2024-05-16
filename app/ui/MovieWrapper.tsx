dc8bcb60b64f0d6a8cb7cca6d95f2f07d9cc8add
eniola <eniolafaithful57@gmail.com>
2024-05-16 21:51:26 +0100

    fixed the problem of state management conflict

--- app/ui/MovieWrapper.tsx
+++ app/ui/MovieWrapper.tsx
@@ -5,54 +5,32 @@ import MovieCard from "./movie-card";
 import axios from "axios";
 import { Context } from "../contexts/GenreProvider";
 import Loader from "./Loader";
+import { useFetchMovies } from "../hooks/fetchMovies";
 
-const url =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-
-export default function MovieWrapper({ movies }: { movies: Movies[] }) {
+// { movies }: { movies: Movies[] }
+export default function MovieWrapper() {
   const { genreId, loading, setLoading } = useContext(
     Context
   ) as GenreContextType;
-  const [moviesGenreFilter, setMovies] = useState<Movies[]>([]);
 
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await axios.get(`${url}&with_genres=${genreId}`, {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_READ_ACCESS_TOKEN}`,
-        },
-      });
-      console.log(response.data.results);
-      setMovies(response.data.results);
-      setLoading(false);
-    }
-    fetchMovies();
-  }, [genreId]);
+  const { moviesGenreFilter } = useFetchMovies();
 
   return (
-    <div className="mx-[15px] space-y-[10px]">
+    <div>
       {loading && <Loader />}
 
-      {!loading &&
-        moviesGenreFilter.length === 0 &&
-        movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            src={movie.poster_path}
-            rating={movie.vote_average}
-            title={movie.title}
-          />
-        ))}
-      {!loading &&
-        moviesGenreFilter.length > 0 &&
-        moviesGenreFilter.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            src={movie.poster_path}
-            rating={movie.vote_average}
-            title={movie.title}
-          />
-        ))}
+      <div className="mx-[15px] flex flex-col gap-4 ">
+        {!loading &&
+          moviesGenreFilter.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              src={movie.poster_path}
+              rating={movie.vote_average}
+              title={movie.title}
+              id={movie.id}
+            />
+          ))}
+      </div>
     </div>
   );
 }
