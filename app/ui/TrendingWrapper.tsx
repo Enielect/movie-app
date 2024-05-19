1f29eb5edc35d0c2141fd3b632a82ca2ad7eface
eniola <eniolafaithful57@gmail.com>
2024-05-19 21:37:35 +0100

    encapsulated the Trending component so i can make it a client component

---
+++ app/ui/TrendingWrapper.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import MovieCard from "../ui/movie-card";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "../ui/Loader";
+import { useFetchMoreMovies } from "../hooks/fetchMoreMovies";
+import { BASE_URL } from "@/tmdb";
+import { useState } from "react";
+
+export default function TrendingWrapper({ movies }: { movies: Movies[] }) {
+  const popularUrl = `${BASE_URL}/movie/popular?language=en-US`;
+  const [TrendingMovies, setTrendingMovies] = useState(movies);
+  const { hasMore, fetchMoreData } = useFetchMoreMovies(TrendingMovies, setTrendingMovies);
+  return (
+    <div className="p-[20px]">
+      {/* <GoBack /> */}
+      <h1 className="text-2xl py-[20px]">Trending Movies</h1>
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={() => fetchMoreData(popularUrl)}
+        hasMore={hasMore}
+        loader={<Loader />}
+      >
+        {movies.map((movie: Movies) => (
+          <MovieCard
+            key={movie.id}
+            id={movie.id}
+            src={movie.poster_path}
+            rating={movie.vote_average}
+            title={movie.title}
+            className="mx-[14px]"
+          />
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+}
