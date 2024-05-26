a29726a3a6b8245755ca77dc16c5f952505afac6
eniola <eniolafaithful57@gmail.com>
2024-05-26 19:19:20 +0100

    chore: Remove unused code and update dependencies

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,10 @@
 import { auth, signOut } from "@/auth";
-import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default async function Page() {
   const session = await auth();
-  const [guestSessionId, setGuestSessionId] = useState(null);
 
-  useEffect(() => {
-    if (session?.user) {
-      // replace 'your-tmdb-api-key' with your actual TMDB API key
-      axios
-        .get(
-          `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${process.env.TMDB_API_KEY}`
-        )
-        .then((response) => {
-          setGuestSessionId(response.data.guest_session_id);
-        })
-        .catch((error) => {
-          console.error("Error creating guest session id:", error);
-        });
-    }
-  }, [session]);
   return (
     <div className="w-full h-full">
       <h1 className="text-[18px] font-[18px] text-center my-[50px]">
