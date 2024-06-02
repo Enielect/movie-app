2e62b8fa3f96a301484dd14494037634ced52cb9
eniola <eniolafaithful57@gmail.com>
2024-06-02 19:52:06 +0100

    implemented side-menu for a scroll-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,34 @@
 import GenreList from "@/app/ui/mobile/genre-list";
 import SearchMobile from "@/app/ui/mobile/search";
-import { fetchPopularMovies } from "@/tmdb";
-import axios, { Axios } from "axios";
-import GenreProvider from "./contexts/GenreProvider";
 import MovieWrapper from "./ui/MovieWrapper";
 import { Suspense } from "react";
 import Loader from "./ui/Loader";
+import SideMenu from "./ui/mobile/side-menu";
 
 //wnat i noticed here, you can't use async/await in a client component.
 
 /**
  * Renders the Page component.
- * 
+ *
  * @returns The rendered Page component.
  */
 export default async function Page() {
-  // console.log(movies);
-
   return (
-    <div className="bg-[#03030a] overflow-hidden">
+    <div className="bg-[#03030a] md:grid overflow-hidden md:grid-cols-[250px_auto]">
       {/* included the genreprovider to update the genre id on click of a particular genre button */}
       {/* add the side menu */}
-      <Suspense fallback={<Loader />}>
-        <SearchMobile />
-      </Suspense>
-      <GenreList />
-      <div className="overflow-auto">
-        <MovieWrapper />
+      
+      <div className="hidden md:block">
+        <SideMenu display="desktop" />
+      </div>
+      <div>
+        <Suspense fallback={<Loader />}>
+          <SearchMobile />
+        </Suspense>
+        <GenreList />
+        <div className="overflow-auto">
+          <MovieWrapper />
+        </div>
       </div>
     </div>
   );
