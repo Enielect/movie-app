c85c76494e3220c9281aca7112b328343b5d5948
eniola <eniolafaithful57@gmail.com>
2024-06-10 20:04:26 +0100

    dynamically determine the remaining height for the moviewrapper

--- app/ui/MovieWrapper.tsx
+++ app/ui/MovieWrapper.tsx
@@ -9,17 +9,32 @@ import { url, useFetchMovies } from "../hooks/fetchMovies";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useFetchMoreMovies } from "../hooks/fetchMoreMovies";
 import useSearchedStore from "../store/searchStore";
+import NetworkError from "./NetworkError";
 
 // { movies }: { movies: Movies[] }
 export default function MovieWrapper() {
   const { loading, genreId } = useContext(Context) as GenreContextType;
-  const genreUrl = `${url}&with_genres=${genreId}`;
-
-  const { moviesGenreFilter, setMovies } = useFetchMovies();
+  const { moviesGenreFilter, setMovies, networkError } = useFetchMovies();
   const { fetchMoreData, hasMore } = useFetchMoreMovies(setMovies);
   const { searchedMovies } = useSearchedStore() as { searchedMovies: Movies[] };
-  useEffect(() => console.log(searchedMovies), [searchedMovies]);
 
+  const [viewportSize, setViewportSize] = useState<number>(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setViewportSize(window.innerHeight);
+    });
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        setViewportSize(window.innerHeight);
+      });
+    };
+  }, []);
+
+  const genreUrl = `${url}&with_genres=${genreId}`;
+
+  if (networkError) return <NetworkError />;
   return (
     <div className="overflow-auto h-full">
       {loading && <Loader />}
@@ -29,7 +44,7 @@ export default function MovieWrapper() {
         next={() => fetchMoreData(genreUrl)}
         loader={<Loader />}
         // style={{height: '40rem'}}
-        height={700}
+        height={viewportSize - 190}
       >
         {/* we were having a problem with lagging in the scrolling and the solution was just to remove the div and render the elements directly */}
         <div className="mx-[15px] md:grid md:grid-cols-3 gap-[15px] lg:grid-cols-4  ">
