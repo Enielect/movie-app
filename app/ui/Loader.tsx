0046daa1680cc2e4fdbffb1a3553001be5c19a7d
eniola <eniolafaithful57@gmail.com>
2024-06-14 09:36:20 +0100

    created a new loader file for another purpose

--- app/ui/Loader.tsx
+++ app/ui/Loader.tsx
@@ -1,9 +1,16 @@
-import styles from "./spinner.module.css";
+import { BounceLoader } from "react-spinners";
 
-export default function Loader() {
+export default function Loading() {
   return (
-    <div className="grid place-items-center">
-      <div className={styles.spinner}></div>
+    <div className="md:grid overflow-hidden h-screen w-screen ">
+      <div className="flex justify-center items-center h-full w-full">
+        <BounceLoader
+          loading={true}
+          color="#ef7e15"
+          aria-label="loading Spinner"
+          size={40}
+        />
+      </div>
     </div>
   );
 }
