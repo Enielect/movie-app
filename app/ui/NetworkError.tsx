e2df60a288f55e53c52a0971a4e356cd3753bf25
eniola <eniolafaithful57@gmail.com>
2024-06-16 09:37:54 +0100

    Tried to manage network errors in my application

---
+++ app/ui/NetworkError.tsx
@@ -0,0 +1,8 @@
+export default function NetworkError() {
+  return (
+    <div className="w-50vw h-50vh grid place-items-center bg-white text-xl">
+      There is an issue with your network, check your internet connection and
+      try again
+    </div>
+  );
+}
