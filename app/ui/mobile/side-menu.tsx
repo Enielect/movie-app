d6713271983f58faf1c6727b0d687379925164ee
eniola <eniolafaithful57@gmail.com>
2024-06-17 09:40:27 +0100

    Drastically refactored my sidemenu to be compatible with both desktop and mobile

--- app/ui/mobile/side-menu.tsx
+++ app/ui/mobile/side-menu.tsx
@@ -1,31 +1,41 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function SideMenu({
   setIsModalOpen,
-  currentRoute,
+  display,
 }: {
-  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  currentRoute: string;
+  setIsModalOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  display: string;
 }) {
+  const pathname = usePathname();
+  const currentRoute = pathname.split("/")[1];
+  console.log(pathname === '/')
   const Links = ["Genres", "Trending", "Upcoming", "Favourites"];
+
+  const buttonProps: { onClick?: () => void } = {};
+  if (display !== "desktop") {
+    buttonProps.onClick = () => setIsModalOpen((c) => !c);
+  }
   return (
-    <div className="py-[20px] flex flex-col justify-between items-center px-[20px] fixed top-0 left-0 w-screen h-[100dvh] bg-black bg-opacity-90 z-10">
-      <Link
-        href="/"
-        aria-label="logo"
-        onClick={() => setIsModalOpen((c) => !c)}
-      >
+    <div
+      className={`py-[40px] flex flex-col justify-between items-center px-[20px] h-[100dvh] md:bg-[#03030a] top-0 left-0 ${display === 'mobile' && "w-screen h-[100dvh] fixed"} bg-black bg-opacity-90 z-10`}
+    >
+      <Link href="/" aria-label="logo" {...buttonProps}>
         <Image src="/logo.png" width={80} height={80} alt="logo" />
       </Link>
       <ul aria-label="links" className="w-full space-y-3">
         {Links.map((link) => (
           <li
             key={link}
-            onClick={() => setIsModalOpen((c) => !c)}
+            {...buttonProps}
             className={`px-[20px] py-[10px] rounded-md w-full text-center ${
-              (!currentRoute && link.toLowerCase() == "genres") ||
-              (currentRoute == link.toLowerCase() && "bg-orange-500 border-orange-500")
+              ((pathname === "/" && link.toLowerCase() == "genres") ||
+              (currentRoute == link.toLowerCase())) &&
+                "bg-orange-500 border-orange-500"
             }  hover:bg-orange-500   hover:border-orange-500 hover:bg-opacity-40 bg-gray-500 bg-opacity-40 border`}
           >
             <Link
@@ -38,44 +48,12 @@ export default function SideMenu({
             </Link>
           </li>
         ))}
-        {/* <li
-          onClick={() => setIsModalOpen((c) => !c)}
-          className="px-[20px] py-[10px] rounded-md w-full text-center  hover:bg-orange-500   hover:border-orange-500 hover:bg-opacity-40 bg-gray-500 bg-opacity-40 border"
-        >
-          <Link className="w-full inline-block" href={"/"}>
-            Genres
-          </Link>
-        </li>
-        <li
-          onClick={() => setIsModalOpen((c) => !c)}
-          className="px-[20px] py-[10px] rounded-md text-center w-full hover:bg-orange-500 hover:bg-opacity-40  hover:border-orange-500 border bg-gray-500 bg-opacity-40"
-        >
-          <Link className="w-full inline-block" href={"/trending"}>
-            Trending
-          </Link>
-        </li>
-        <li
-          onClick={() => setIsModalOpen((c) => !c)}
-          className="px-[20px] py-[10px] rounded-md text-center w-ful hover:bg-orange-500 hover:bg-opacity-40 hover:border-orange-500 border bg-gray-500 bg-opacity-40"
-        >
-          <Link className="w-full inline-block" href={"/upcoming"}>
-            Upcoming
-          </Link>
-        </li>
-        <li
-          onClick={() => setIsModalOpen((c) => !c)}
-          className="px-[20px] py-[10px] rounded-md text-center w-full hover:bg-orange-500 hover:bg-opacity-40 hover:border-orange-500 border bg-gray-500 bg-opacity-40"
-        >
-          <Link className="w-full inline-block" href={"/favourites"}>
-            Favourites
-          </Link>
-        </li> */}
       </ul>
       <div
         className="px-[20px] py-[10px] rounded-md text-center w-full hover:bg-orange-500 hover:border-orange-500 border bg-orange-600 bg-opacity-40"
         aria-label="login"
       >
-        <div onClick={() => setIsModalOpen((c) => !c)} className="w-full">
+        <div {...buttonProps} className="w-full">
           <Link className="w-full block" href={"/login"}>
             {" "}
             Log in
